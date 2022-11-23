8339a4ba60f600f1dbaccf42b2c7ed7633e0808b
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-11-23 02:15:32 +0200

    Render annotations from DB

--- app/models/decidim/participatory_documents/annotation.rb
+++ app/models/decidim/participatory_documents/annotation.rb
@@ -11,6 +11,20 @@ module Decidim
       def self.log_presenter_class_for(_log)
         Decidim::ParticipatoryDocuments::AdminLog::AnnotationPresenter
       end
+
+      def serialize
+        {
+          annotationType: annotation_type,
+          color: color.map(&:to_i),
+          thickness: thickness,
+          opacity: opacity,
+          scale: scale,
+          paths: paths.map(&:to_i),
+          pageIndex: page_index,
+          rect: rect.map(&:to_i),
+          rotation: rotation
+        }
+      end
     end
   end
 end
