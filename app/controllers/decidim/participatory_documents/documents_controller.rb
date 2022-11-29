09d192bb1b4b8d043e4d587ffd15b8cd4ce9ca1b
Ivan Vergés <ivan@pokecode.net>
2022-11-29 14:10:54 +0100

    add proof of concept viewer

--- app/controllers/decidim/participatory_documents/documents_controller.rb
+++ app/controllers/decidim/participatory_documents/documents_controller.rb
@@ -3,7 +3,17 @@
 module Decidim
   module ParticipatoryDocuments
     class DocumentsController < Decidim::ParticipatoryDocuments::ApplicationController
+      helper_method :document
+      
       def index; end
+
+      def pdf_viewer
+        render layout: false
+      end
+
+      def document
+        @document ||= Decidim::ParticipatoryDocuments::Document.find_by(component: current_component)
+      end
     end
   end
 end
