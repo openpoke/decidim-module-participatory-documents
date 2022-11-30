1a7f327f953d3f4918a051295eab4cea53cca875
Ivan Vergés <ivan@pokecode.net>
2022-11-30 22:49:02 +0100

    fix move boxes

--- app/packs/src/decidim/participatory_documents/box_area.js
+++ app/packs/src/decidim/participatory_documents/box_area.js
@@ -2,6 +2,7 @@ export default class BoxArea {
 	constructor(div) {
 		this.div = div;
 		this.id = this.div.id = "box-" + Date.now();
+		this.div.draggable = false;
 		this.layer = this.div.parentElement;
 		this.moving = false;
 		this.top = 0;
@@ -11,12 +12,34 @@ export default class BoxArea {
 	}
 
 	init() {
-		this.div.addEventListener("mousedown", (e) => this.onMouseDown(e));
-		this.div.addEventListener("mousemove", (e) => this.onMouseMove(e));
-		this.div.addEventListener("mouseup", (e) => this.onMouseUp(e));
-		this.div.addEventListener("mouseleave", (e) => this.onMouseUp(e));
-		// this.div.addEventListener("mouseout", (e) => this.onMouseUp(e));
+		this.div.addEventListener("mousedown", (e) => this.onMouseDown(e), true);
+		this.div.addEventListener("mousemove", (e) => this.onMouseMove(e), true);
+		this.div.addEventListener("mouseup", (e) => this.onMouseUp(e), true);
+		this.div.addEventListener("mouseleave", (e) => this.onMouseUp(e), true);
+		// this.div.addEventListener("mouseout", (e) => this.onMouseUp(e), true);
+		// this.div.addEventListener("dragstart", (e) => this.onDragStart(e), true);
+		// this.div.addEventListener("dragend", (e) => this.onDragEnd(e), true);
 	}
+
+	// onDragStart(e) {
+	// 	e.stopPropagation();
+	//   this.left = this.layer.getBoundingClientRect().x + e.layerX;
+	//   this.top = this.layer.getBoundingClientRect().y + e.layerY;
+	// 	console.log("box dragstart", e, this.div);
+	// }
+
+	// onDragEnd(e) {
+	// 	e.stopPropagation();
+  //   let w = e.clientX - this.left - 3;
+  //   let h = e.clientY - this.top - 3;
+  //   let mousePercentLeft = (100 * w/this.layer.clientWidth)
+  //   let mousePercentTop = (100 * h/this.layer.clientHeight)
+  //   this.div.style.left = mousePercentLeft + "%";
+  //   this.div.style.top = mousePercentTop + "%";
+	// 	this.div.classList.remove("grabbing");
+	// 	this.unblockSibilings();
+	// 	console.log("box end", e, this.div);
+	// }
 
 	onMouseDown(e) {
 		e.stopPropagation();
@@ -43,16 +66,19 @@ export default class BoxArea {
 
 	onMouseUp(e) {
 		e.stopPropagation();
-		this.div.classList.remove("grabbing");
 		this.moving = false;
+		this.div.classList.remove("grabbing");
 		this.unblockSibilings();
+	  // console.log("box mouseup", this.div.style, e);
 	}
 
 	blockSibilings() {
+		this.layer.classList.add("blocked")
 		this.layer.querySelectorAll(".box").forEach(div => div.id != this.id && div.classList.add("blocked"));
 	}
 
 	unblockSibilings() {
+		this.layer.classList.remove("blocked")
 		this.layer.querySelectorAll(".box").forEach(div => div.classList.remove("blocked"));
 	}
 }
