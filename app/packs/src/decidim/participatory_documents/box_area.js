a7d17ccdb67ca5d78882d2cbc1cb4f41bfc07398
Ivan Vergés <ivan@pokecode.net>
2022-12-01 00:30:36 +0100

    add resize

--- app/packs/src/decidim/participatory_documents/box_area.js
+++ app/packs/src/decidim/participatory_documents/box_area.js
@@ -2,7 +2,7 @@ export default class BoxArea {
 	constructor(div) {
 		this.div = div;
 		this.id = this.div.id = "box-" + Date.now();
-		this.div.draggable = false;
+		this.div.draggable = true;
 		this.layer = this.div.parentElement;
 		this.moving = false;
 		this.top = 0;
@@ -12,34 +12,38 @@ export default class BoxArea {
 	}
 
 	init() {
-		this.div.addEventListener("mousedown", (e) => this.onMouseDown(e), true);
-		this.div.addEventListener("mousemove", (e) => this.onMouseMove(e), true);
-		this.div.addEventListener("mouseup", (e) => this.onMouseUp(e), true);
-		this.div.addEventListener("mouseleave", (e) => this.onMouseUp(e), true);
-		// this.div.addEventListener("mouseout", (e) => this.onMouseUp(e), true);
-		// this.div.addEventListener("dragstart", (e) => this.onDragStart(e), true);
-		// this.div.addEventListener("dragend", (e) => this.onDragEnd(e), true);
+		this.resize = new ResizeObserver(this.onResize.bind(this));
+		this.resize.observe(this.div);
+		// setTimeout(()=>{
+		// 	this.div.style.resize = "both;"
+		// },1000);
+		this.div.addEventListener("mousedown",this.onMouseDown.bind(this), true);
+		// Alternate to "dragend". Has problems with resizing
+		// this.div.addEventListener("mousemove", (e) => this.onMouseMove(e), true);
+		this.div.addEventListener("mouseup", this.onMouseUp.bind(this), true);
+		this.div.addEventListener("mouseleave", this.onMouseUp.bind(this), true);
+		// Alternate to "mousemove"
+		this.div.addEventListener("dragend", this.onDragEnd.bind(this), true);
 	}
 
-	// onDragStart(e) {
-	// 	e.stopPropagation();
-	//   this.left = this.layer.getBoundingClientRect().x + e.layerX;
-	//   this.top = this.layer.getBoundingClientRect().y + e.layerY;
-	// 	console.log("box dragstart", e, this.div);
-	// }
+	onResize(e,b) {
+		this.div.style.width = (100 * e[0].contentRect.width / this.layer.clientWidth) + "%";
+		this.div.style.height = (100 * e[0].contentRect.height / this.layer.clientHeight) + "%";
+		// console.log("box resize", e,b)
+	}
 
-	// onDragEnd(e) {
-	// 	e.stopPropagation();
-  //   let w = e.clientX - this.left - 3;
-  //   let h = e.clientY - this.top - 3;
-  //   let mousePercentLeft = (100 * w/this.layer.clientWidth)
-  //   let mousePercentTop = (100 * h/this.layer.clientHeight)
-  //   this.div.style.left = mousePercentLeft + "%";
-  //   this.div.style.top = mousePercentTop + "%";
-	// 	this.div.classList.remove("grabbing");
-	// 	this.unblockSibilings();
-	// 	console.log("box end", e, this.div);
-	// }
+	onDragEnd(e) {
+		e.stopPropagation();
+    let w = e.clientX - this.left - 3;
+    let h = e.clientY - this.top - 3;
+    let mousePercentLeft = (100 * w/this.layer.clientWidth)
+    let mousePercentTop = (100 * h/this.layer.clientHeight)
+    this.div.style.left = mousePercentLeft + "%";
+    this.div.style.top = mousePercentTop + "%";
+		this.div.classList.remove("grabbing");
+		this.unblockSibilings();
+		// console.log("box end", e, this.div);
+	}
 
 	onMouseDown(e) {
 		e.stopPropagation();
