895db6a36999b5d537a4ac674efe59d6c749bd32
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-12-05 13:38:02 +0200

    Add Seeds for Document, Zone, Annotation

--- db/migrate/20221205093306_change_decidim_participatory_documents_annotations.rb
+++ db/migrate/20221205093306_change_decidim_participatory_documents_annotations.rb
@@ -3,7 +3,7 @@
 class ChangeDecidimParticipatoryDocumentsAnnotations < ActiveRecord::Migration[6.0]
   def change
     remove_column :decidim_participatory_documents_annotations, :data
-    add_column :decidim_participatory_documents_annotations, :paths, :jsonb, default: {}
+    add_column :decidim_participatory_documents_annotations, :rect, :jsonb, default: {}
     add_column :decidim_participatory_documents_annotations, :page_number, :integer, default: 1
     add_reference :decidim_participatory_documents_annotations, :zone, index: true, foreign_key: { to_table: :decidim_participatory_documents_zones }
   end
