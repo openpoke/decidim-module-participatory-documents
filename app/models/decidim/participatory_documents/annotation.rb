7cfca3a725131d4f6601d06e6a3b0d36b631a191
Alexandru-Emil Lupu <contact@alecslupu.ro>
2022-12-05 14:04:25 +0200

    Add Admin log for Annotations

--- app/models/decidim/participatory_documents/annotation.rb
+++ app/models/decidim/participatory_documents/annotation.rb
@@ -4,6 +4,11 @@ module Decidim
   module ParticipatoryDocuments
     class Annotation < ApplicationRecord
       belongs_to :zone, class_name: "Decidim::ParticipatoryDocuments::Zone"
+
+
+      def self.log_presenter_class_for(_log)
+        Decidim::ParticipatoryDocuments::AdminLog::AnnotationPresenter
+      end
     end
   end
 end
---
+++ app/presenters/decidim/participatory_documents/admin_log/annotation_presenter.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryDocuments
+    module AdminLog
+      class AnnotationPresenter < Decidim::Log::BasePresenter
+        def action_string
+          "decidim.admin_log.annotation.#{action}"
+        end
+
+        private
+        def i18n_params
+          super.merge({
+                        document_name: document_presenter.present
+                      })
+        end
+
+        def document_presenter
+          Decidim::Log::ResourcePresenter.new(action_log.resource.zone.document, h, action_log.resource.zone.document)
+        end
+      end
+    end
+  end
+end
