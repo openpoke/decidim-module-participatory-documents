e966359c50fc37834907fa352eb3941b6a3c4049
Anna Topalidi <t.opalidisanna@gmail.com>
2022-12-10 19:45:40 +0100

    add tests

--- app/helpers/decidim/participatory_documents/admin/documents_helper.rb
+++ app/helpers/decidim/participatory_documents/admin/documents_helper.rb
@@ -5,18 +5,51 @@ module Decidim
     module Admin
       module DocumentsHelper
         def pdf_manage_button(document)
-          edit_title = t("actions.edit_pdf", scope: "decidim.participatory_documents")
-          new_title = t("actions.new", scope: "decidim.participatory_documents")
+          if document.blank? && allowed_to?(:create, :participatory_document)
+            new_pdf_btn
+          elsif document.file.attached? && allowed_to?(:update, :participatory_document)
+            edit_pdf_btn
+          else
+            edit_document_btn
+          end
+        end
 
-          title = document.present? && document.file.attached? ? edit_title : new_title
+        private
 
-          href = document.present? && document.file.attached? ? edit_pdf_documents_path(id: document.id) : new_document_path
+        def btn_icon
+          icon("document", class: "icon--document icon icon icon-document mr-xs", aria_label: btn_title, role: "img")
+        end
 
-          content_icon = icon("document", class: "icon--document icon icon icon-document mr-xs", aria_label: title, role: "img")
+        def btn_title
+          return t("actions.new", scope: "decidim.participatory_documents") unless document.present?
 
+          if document.file.attached?
+            t("actions.edit_pdf", scope: "decidim.participatory_documents")
+          else
+            t("actions.edit_document", scope: "decidim.participatory_documents")
+          end
+        end
+
+        def new_pdf_btn
           content_tag(:div, class: "flex--cc flex-gap--1") do
-            content_tag(:a, title: title, href: href, class: "button button--simple") do
-              content_icon + content_tag(:span, title)
+            content_tag(:a, title: btn_title, href: new_document_path, class: "button button--simple") do
+              btn_icon + content_tag(:span, btn_title)
+            end
+          end
+        end
+
+        def edit_pdf_btn
+          content_tag(:div, class: "flex--cc flex-gap--1") do
+            content_tag(:a, title: btn_title, href: edit_pdf_documents_path(id: document.id), class: "button button--simple") do
+              btn_icon + content_tag(:span, btn_title)
+            end
+          end
+        end
+
+        def edit_document_btn
+          content_tag(:div, class: "flex--cc flex-gap--1") do
+            content_tag(:a, title: btn_title, href: edit_document_path(id: document.id), class: "button button--simple") do
+              btn_icon + content_tag(:span, btn_title)
             end
           end
         end
---
+++ spec/system/admin/admin_manages_participatory_documents_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Admin manages participatory documents", type: :system do
+  let(:manifest_name) { "participatory_documents" }
+  let(:organization) { participatory_process.organization }
+  let!(:user) { create :user, :admin, :confirmed, organization: organization }
+
+  def upload_file
+    document.file.attach(io: File.open(Decidim::Dev.asset("Exampledocument.pdf")), filename: "Exampledocument.pdf")
+  end
+
+  include_context "when managing a component as an admin"
+
+  context "when document is not created" do
+    it "shows the upload button" do
+      visit_component_admin
+
+      expect(page).to have_content("Upload")
+    end
+  end
+
+  context "when a file is not uploaded" do
+    let!(:document) { create :participatory_documents_document, component: component }
+
+    it "shows the upload button" do
+      visit_component_admin
+
+      expect(page).to have_content("Edit document")
+    end
+  end
+
+  context "when a file is uploaded" do
+    let!(:document) { create :participatory_documents_document, component: component }
+
+    before do
+      upload_file
+    end
+
+    it "shows the edit button" do
+      visit_component_admin
+
+      expect(page).to have_content("Edit PDF")
+    end
+  end
+end
