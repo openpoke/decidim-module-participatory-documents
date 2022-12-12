7e53c53f19dcd92de95ade574b066f8602b13815
Anna Topalidi <t.opalidisanna@gmail.com>
2022-12-12 16:18:55 +0100

    add edit doc button for upload

--- app/helpers/decidim/participatory_documents/admin/documents_helper.rb
+++ app/helpers/decidim/participatory_documents/admin/documents_helper.rb
@@ -8,7 +8,9 @@ module Decidim
           if document.blank? && allowed_to?(:create, :participatory_document)
             new_pdf_btn
           elsif document.file.attached? && allowed_to?(:update, :participatory_document)
-            edit_pdf_btn
+            content_tag(:div, class: "flex--cc flex-gap--1") do
+               edit_document_btn + edit_pdf_btn
+            end
           else
             edit_document_btn
           end
@@ -16,40 +18,37 @@ module Decidim
 
         private
 
-        def btn_icon
-          icon("document", class: "icon--document icon icon icon-document mr-xs", aria_label: btn_title, role: "img")
-        end
-
-        def btn_title
-          return t("actions.new", scope: "decidim.participatory_documents") if document.blank?
-
-          if document.file.attached?
-            t("actions.edit_pdf", scope: "decidim.participatory_documents")
-          else
-            t("actions.edit_document", scope: "decidim.participatory_documents")
-          end
+        def btn_icon(label)
+          icon("document", class: "icon--document icon icon icon-document mr-xs", aria_label: label, role: "img")
         end
 
         def new_pdf_btn
+          btn_title = t("actions.new", scope: "decidim.participatory_documents")
+
+
           content_tag(:div, class: "flex--cc flex-gap--1") do
             content_tag(:a, title: btn_title, href: new_document_path, class: "button button--simple") do
-              btn_icon + content_tag(:span, btn_title)
+              btn_icon(btn_title) + content_tag(:span, btn_title)
             end
           end
         end
 
         def edit_pdf_btn
+          btn_title = t("actions.edit_pdf", scope: "decidim.participatory_documents")
+
           content_tag(:div, class: "flex--cc flex-gap--1") do
-            content_tag(:a, title: btn_title, href: edit_pdf_documents_path(id: document.id), class: "button button--simple") do
-              btn_icon + content_tag(:span, btn_title)
+            content_tag(:a, title: btn_title, href: edit_pdf_documents_path(id: document.id), class: "button small light success") do
+              btn_icon(btn_title) + content_tag(:span, btn_title)
             end
           end
         end
 
         def edit_document_btn
+          btn_title = t("actions.edit_document", scope: "decidim.participatory_documents")
+
           content_tag(:div, class: "flex--cc flex-gap--1") do
-            content_tag(:a, title: btn_title, href: edit_document_path(id: document.id), class: "button button--simple") do
-              btn_icon + content_tag(:span, btn_title)
+            content_tag(:a, title: btn_title, href: edit_document_path(id: document.id), class: "button small light") do
+              btn_icon(btn_title) + content_tag(:span, btn_title)
             end
           end
         end
