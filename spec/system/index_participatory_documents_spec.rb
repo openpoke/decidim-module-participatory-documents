8ad6a444efc34072865bf0b88f4787c079e4b1a0
Anna Topalidi <t.opalidisanna@gmail.com>
2023-01-09 02:04:04 +0100

    add: optional title and description, add spec

--- spec/system/index_participatory_documents_spec.rb
+++ spec/system/index_participatory_documents_spec.rb
@@ -4,12 +4,36 @@ require "spec_helper"
 
 describe "Index participatory_documents", type: :system do
   include_context "with a component"
+  let(:organization) { create :organization }
+  let(:participatory_process) { create :participatory_process, organization: organization }
   let(:manifest_name) { "participatory_documents" }
-  let!(:document) { create :participatory_documents_document, :with_file, component: component }
+  let!(:component) { create :participatory_documents_component, participatory_space: participatory_process }
+  let!(:title) { "Test title" }
+  let!(:description) { "Test description" }
+  let!(:document) { create :participatory_documents_document, :with_file, title: { en: title }, description: {en: description }, component: component }
+
+  before do
+    visit_component
+  end
 
   it "shows the index page" do
-    visit_component
-
     expect(page).to have_css("iframe", id: "pdf-iframe")
+  end
+
+  context "when the document has no title or description" do
+    let(:title) { nil }
+    let(:description) { nil }
+
+    it "does not show title and description" do
+      expect(page).not_to have_content("Test title")
+      expect(page).not_to have_content("Test description")
+    end
+  end
+
+  context "when the document has a title and a description" do
+    it "show title and description" do
+      expect(page).to have_content("Test title")
+      expect(page).to have_content("Test description")
+    end
   end
 end
