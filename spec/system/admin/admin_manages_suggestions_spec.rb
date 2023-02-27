a20bf4e47089f1bbc58692ed73760eaf9c5253a0
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-02-28 00:02:00 +0200

    Add Author and Section filters (#71)
    * Add author filters
    
    * Add section filters

--- spec/system/admin/admin_manages_suggestions_spec.rb
+++ spec/system/admin/admin_manages_suggestions_spec.rb
@@ -3,6 +3,7 @@
 require "spec_helper"
 
 describe "Admin manages participatory documents", type: :system do
+  include Decidim::TranslationsHelper
   let(:manifest_name) { "participatory_documents" }
   let(:router) { Decidim::EngineRouter.admin_proxy(document.component).decidim_admin_participatory_process_participatory_documents }
 
@@ -14,7 +15,8 @@ describe "Admin manages participatory documents", type: :system do
 
   let(:section1) { create(:participatory_documents_section, document: document) }
   let(:section2) { create(:participatory_documents_section, document: document) }
-  let!(:document_suggestions) { create_list(:participatory_documents_suggestion, 10, suggestable: document) }
+  let!(:single_document_suggestion) { create(:participatory_documents_suggestion, suggestable: document) }
+  let!(:document_suggestions) { create_list(:participatory_documents_suggestion, 9, suggestable: document) }
   let!(:section1_suggestions) { create_list(:participatory_documents_suggestion, 10, suggestable: section1) }
   let!(:section2_suggestions) { create_list(:participatory_documents_suggestion, 10, suggestable: section2) }
 
@@ -34,6 +36,69 @@ describe "Admin manages participatory documents", type: :system do
       document_suggestions.each do |suggestion|
         expect(page).to have_content(suggestion.author.name)
       end
+    end
+  end
+
+  context "when sorting by author's name" do
+    let(:dummy_user) { create(:user, name: "zzz-user 1", organization: organization) }
+    let!(:single_document_suggestion) { create(:participatory_documents_suggestion, suggestable: document, author: dummy_user) }
+
+    it "sorts ascendent" do
+      expect(page).to have_content(dummy_user.name)
+      click_link "Author"
+      expect(page).not_to have_content(dummy_user.name)
+    end
+
+    it "sorts descendent" do
+      expect(page).to have_content(dummy_user.name)
+      click_link "Author"
+      click_link "Author"
+      expect(page).to have_content(dummy_user.name)
+    end
+  end
+
+  it "filters by author's name" do
+    expect(page).to have_content(document_suggestions.first.author.name)
+    expect(page).to have_content(document_suggestions.last.author.name)
+    within ".filters__section" do
+      find("a.dropdown", text: "Filter").hover
+      find("a", text: "Author").hover
+      find("a", text: document_suggestions.last.author.name).click
+    end
+    expect(page).not_to have_content(document_suggestions.first.author.name)
+    expect(page).to have_content(document_suggestions.last.author.name)
+  end
+
+  context "when sorting by section's name" do
+    let(:section1) { create(:participatory_documents_section, document: document, title: { en: "zzzz-section" }) }
+
+    it "sorts ascendent" do
+      expect(page).to have_content(translated_attribute(section1.title))
+      click_link "Section"
+      expect(page).not_to have_content(translated_attribute(section1.title))
+    end
+
+    it "sorts descendent" do
+      expect(page).to have_content(translated_attribute(section1.title))
+      click_link "Author"
+      click_link "Author"
+      expect(page).to have_content(translated_attribute(section1.title))
+    end
+  end
+
+  it "filters by section name" do
+    within ".container" do
+      expect(page).to have_content("Global")
+      expect(page).to have_content(translated_attribute(section1.title))
+    end
+    within ".filters__section" do
+      find("a.dropdown", text: "Filter").hover
+      find("a", text: "Section").hover
+      find("a", text: translated_attribute(section1.title)).click
+    end
+    within ".container" do
+      expect(page).not_to have_content("Global")
+      expect(page).to have_content(translated_attribute(section1.title))
     end
   end
 
