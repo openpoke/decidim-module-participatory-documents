42496f385cc0c99d9086f2accb8118cd92e492bd
Anna Topalidi <t.opalidisanna@gmail.com>
2023-03-20 21:30:47 +0100

    add answered_at

--- app/cells/decidim/participatory_documents/suggestion_cell.rb
+++ app/cells/decidim/participatory_documents/suggestion_cell.rb
@@ -11,6 +11,12 @@ module Decidim
         render
       end
 
+      def answered_at
+        return unless has_answer?
+
+        l(model.answered_at, format: :decidim_short)
+      end
+
       def humanize_suggestion_state
         I18n.t(model.state, scope: "decidim.participatory_documents.suggestions.answers")
       end
