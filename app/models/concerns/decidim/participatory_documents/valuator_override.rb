deabd70d36d54c45aea6574f82f6549777051363
Ivan Vergés <ivan@pokecode.net>
2023-03-21 13:07:01 +0100

    compatibilizy valuator override with other modules (#79)

--- app/models/concerns/decidim/participatory_documents/valuator_override.rb
+++ app/models/concerns/decidim/participatory_documents/valuator_override.rb
@@ -6,8 +6,11 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
+        # it is important to ensure that the aliased method name is unique in case of other modules are doing the same
+        alias_method :decidim_participatory_documents_original_accepted_components, :accepted_components
+
         def accepted_components
-          [:proposals, :participatory_documents]
+          decidim_participatory_documents_original_accepted_components + [:participatory_documents]
         end
       end
     end
