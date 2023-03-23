1ddc53a989c179129e6ad1d6b6d11bdb209386fc
Anna Topalidi <t.opalidisanna@gmail.com>
2023-03-23 09:43:57 +0100

    change the public side for an unpublished document, add spec

--- app/controllers/decidim/participatory_documents/documents_controller.rb
+++ app/controllers/decidim/participatory_documents/documents_controller.rb
@@ -6,15 +6,6 @@ module Decidim
       include NeedsPdfDocument
       helper Decidim::LayoutHelper
 
-      before_action do
-        raise ActiveRecord::RecordNotFound unless document.present? && document.file.attached?
-      end
-
-      rescue_from ActiveRecord::RecordNotFound do |_exception|
-        flash.now[:alert] = t("documents.missing", scope: "decidim.participatory_documents")
-        redirect_to "/404"
-      end
-
       def index; end
 
       def pdf_viewer
