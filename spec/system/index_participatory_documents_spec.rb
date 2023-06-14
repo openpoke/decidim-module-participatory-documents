8185e5f3eb6fbb2e28c15477fdca5aa7caa93801
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2023-06-14 12:43:48 +0200

    change the public side for an unpublished document (#81)
    * change the public side for an unpublished document, add spec
    
    * change view, change spec

--- spec/system/index_participatory_documents_spec.rb
+++ spec/system/index_participatory_documents_spec.rb
@@ -5,22 +5,35 @@ require "spec_helper"
 describe "Index participatory_documents", type: :system do
   include_context "with a component"
   let(:organization) { create :organization }
+  let(:user) { create(:user, :admin, :confirmed, organization: organization) }
   let(:participatory_process) { create :participatory_process, organization: organization }
   let(:manifest_name) { "participatory_documents" }
   let!(:component) { create :participatory_documents_component, participatory_space: participatory_process }
+  let!(:component_unpublished) { create :participatory_documents_component, :unpublished, participatory_space: participatory_process }
   let!(:title) { "Test title" }
   let!(:description) { "Test description" }
   let!(:document) { create :participatory_documents_document, :with_file, title: { en: title }, description: { en: description }, component: component }
 
   before do
+    switch_to_host(organization.host)
+    login_as user, scope: :user
     visit_component
   end
 
   context "when document is not uploaded" do
     let!(:document) { nil }
 
-    it_behaves_like "a 404 page" do
-      let(:target_path) { main_component_path(component) }
+    it "shows a message" do
+      expect(page).to have_content("There is no document uploaded yet")
+    end
+  end
+
+  context "when component not published and document is uploaded" do
+    let!(:component) { component_unpublished }
+
+    it "shows a message" do
+      expect(page).not_to have_content("There is no document uploaded yet")
+      expect(page).to have_content(document.title["en"])
     end
   end
 
