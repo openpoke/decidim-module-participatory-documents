73fe33b61f4319d40989d70a7fd3e90cf0e800fa
Anna Topalidi <t.opalidisanna@gmail.com>
2023-06-21 11:30:59 +0200

    change migration name

--- db/migrate/20230616100340_add_published_at_to_decidim_participatory_documents_documents.rb
+++ db/migrate/20230616100340_add_published_at_to_decidim_participatory_documents_documents.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddFinalPublishedAtToDecidimParticipatoryDocumentsDocuments < ActiveRecord::Migration[6.0]
+class AddPublishedAtToDecidimParticipatoryDocumentsDocuments < ActiveRecord::Migration[6.0]
   def change
     add_column :decidim_participatory_documents_documents, :published_at, :datetime, index: true
   end
