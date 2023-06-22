c527fa934a91eb5f5f9126ad2ffecbc099fe6562
Ivan Vergés <ivan@pokecode.net>
2023-06-22 11:52:56 +0200

    add postion in db

---
+++ db/migrate/20230622081831_add_decidim_participatory_documents_box_positions.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class AddDecidimParticipatoryDocumentsBoxPositions < ActiveRecord::Migration[6.0]
+  class Document < ApplicationRecord
+    self.table_name = :decidim_participatory_documents_documents
+    has_many :sections
+    has_many :annotations, through: :sections
+  end
+
+  class Section < ApplicationRecord
+    self.table_name = :decidim_participatory_documents_sections
+    belongs_to :document
+    has_many :annotations
+  end
+
+  class Annotation < ApplicationRecord
+    self.table_name = :decidim_participatory_documents_annotations
+    belongs_to :section
+  end
+
+  def change
+    add_column :decidim_participatory_documents_annotations, :position, :integer, null: false, default: 0
+    add_index :decidim_participatory_documents_annotations, :position
+    add_column :decidim_participatory_documents_sections, :position, :integer, null: false, default: 0
+    add_index :decidim_participatory_documents_sections, :position
+
+    Section.find_each do |section|
+      section.update_column(:position, section.document.sections.where("id < ?", section.id).count + 1)
+    end
+
+    Annotation.find_each do |annotation|
+      annotation.update_column(:position, annotation.section.document.annotations.where("decidim_participatory_documents_annotations.id < ?", annotation.id).count + 1)
+    end
+  end
+end
