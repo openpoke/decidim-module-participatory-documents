d6f80ec5da2a92a142f8252a4fe54fcde298ae2e
Ivan Vergés <ivan@pokecode.net>
2023-07-13 15:55:15 +0200

    add env vars for configuration

--- lib/decidim/participatory_documents/version.rb
+++ lib/decidim/participatory_documents/version.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   module ParticipatoryDocuments
-    VERSION = "0.2.0"
+    VERSION = "0.2.1"
     DECIDIM_VERSION = "0.27.3"
     COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
   end
