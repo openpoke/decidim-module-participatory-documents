1559558808d28b622415f74f86c3068b81dfd588
Anna Topalidi <t.opalidisanna@gmail.com>
2024-03-18 13:21:31 +0100

    change: edit/upload document form

--- app/packs/src/decidim/participatory_documents/participatory_documents_admin.js
+++ app/packs/src/decidim/participatory_documents/participatory_documents_admin.js
@@ -1,3 +1,6 @@
 // Custom scripts for participatory_documents
 import "src/decidim/participatory_documents/admin/manage_bulk_suggestions.js"
 import "src/decidim/participatory_documents/admin/preview_box_color.js"
+import "src/decidim/participatory_documents/pdf/box_controls.js"
+import "src/decidim/participatory_documents/pdf/box_control_group.js"
+import "src/decidim/participatory_documents/pdf/box_control_move.js"
