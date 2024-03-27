c78e091abd1872a04f29bafb880adb66a6e2391a
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2024-03-27 15:37:25 +0100

    Update lib/decidim/participatory_documents/version.rb
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- lib/decidim/participatory_documents/version.rb
+++ lib/decidim/participatory_documents/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   module ParticipatoryDocuments
     VERSION = "0.2.2"
-    DECIDIM_VERSION = "0.28.0"
+    DECIDIM_VERSION = "0.28"
     COMPAT_DECIDIM_VERSION = [">= 0.28", "< 0.29"].freeze
   end
 end
