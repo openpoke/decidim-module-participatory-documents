bc3e2507547ac0f0a8ccf8b48dee95e707ae45fd
Ivan Vergés <ivan@pokecode.net>
2025-03-24 13:28:20 +0100

    fix javascript errors

--- app/packs/src/decidim/participatory_documents/upload_modal_initializer.js
+++ app/packs/src/decidim/participatory_documents/upload_modal_initializer.js
@@ -1,6 +1,6 @@
 import SuggestionUploadModal from "src/decidim/participatory_documents/upload_modal";
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("[UploadModalInitializer] Initializing Suggestion UploadModal...");
-    new SuggestionUploadModal();
+  console.log("[UploadModalInitializer] Initializing Suggestion UploadModal...");
+  window.SuggestionUploadModal = new SuggestionUploadModal();
 });
